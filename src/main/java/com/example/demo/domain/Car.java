902335ee90b85a2325e1b157dec87b8d7f774a7f
hayate-sato <hayate.sato@rakus-partners.co.jp>
2022-05-10 15:34:32 +0900

    first commit

---
+++ src/main/java/com/example/demo/domain/Car.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain;
+
+public class Car {
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
